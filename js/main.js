99b7609392e154d6bdad55153e2cfb89980af72d
Andrew Barrett-Sprot <abarrettsprot@gmail.com>
2018-12-03 01:02:05 +0100

    Fix docs navbar not showing
    There was previously no code to show/hide the navbar on the
    docs, add the fix for that.

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function addNavigationListener() {
+  var navigationToggle = document.getElementById("navigation_toggle");
+  var navbar = document.getElementById("navigation");
+  navigationToggle.onclick = function() {
+      navbar.classList.toggle('mobile-collapsed');
+  };
+}
+window.addEventListener('load', addNavigationListener);
