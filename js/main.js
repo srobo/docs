e02777ccee2762d6398b845efe9613a7e14b32ab
Andrew Barrett-Sprot <abarrettsprot@gmail.com>
2018-12-25 01:24:32 +0100

    remove file added by a bad merge

--- js/main.js
+++
@@ -1,8 +0,0 @@
-function addNavigationListener() {
-  var navigationToggle = document.getElementById("navigation_toggle");
-  var navbar = document.getElementById("navigation");
-  navigationToggle.onclick = function() {
-      navbar.classList.toggle('mobile-collapsed');
-  };
-}
-window.addEventListener('load', addNavigationListener);
