0a432b22e3e1725d2f5a9aa7d652fe257f989622
Peter Law <PeterJCLaw@gmail.com>
2019-11-09 18:17:03 +0000

    Force simulator updates to come from GitHub

--- resources/simulator/update.sh
+++ resources/simulator/update.sh
@@ -17,7 +17,7 @@ do
     unzip -q $zipname -d $platform
     rm $zipname
     cd $platform/robot-sim
-    git pull -q git://git.srobo.org/simulator.git
+    git pull -q https://github.com/srobo/sr-turtle.git
     revision=`git log -1 --oneline`
     cd ..
     zip -q -r $zipname .
